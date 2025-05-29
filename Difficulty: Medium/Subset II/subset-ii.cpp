54c147110975864bd5b07e13d8d042df2feb48fe
Anjali Rawat <132810690+anjalirawattttt@users.noreply.github.com>
2025-05-29 17:59:34 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Subset II/subset-ii.cpp
+++ Difficulty: Medium/Subset II/subset-ii.cpp
@@ -2,21 +2,26 @@
 
 class Solution {
 public:
-    void helper(vector<vector<int>> &ans,vector<int>& nums,int i,vector<int> subset){
-        ans.push_back(subset);                
-            
-        for(int j=i;j<nums.size();j++){
-            if(j>i && nums[j]==nums[j-1])continue;
-            subset.push_back(nums[j]);
-            helper(ans,nums,j+1,subset);
-            subset.pop_back();
+    
+    void helper(vector<vector<int>>& ans,vector<int>& arr,vector<int> subset,int i){
+        if(i==arr.size()){
+            ans.push_back(subset);
+            return;
         }
+        
+        subset.push_back(arr[i]);
+        helper(ans,arr,subset,i+1);
+        subset.pop_back();
+        while(i+1<arr.size() && arr[i]==arr[i+1])i++;
+        helper(ans,arr,subset,i+1);
     }
-
-    vector<vector<int>> printUniqueSubsets(vector<int>& nums) {
+    
+    
+    vector<vector<int>> printUniqueSubsets(vector<int>& arr) {
         vector<vector<int>> ans;
-        sort(nums.begin(),nums.end());
-        helper(ans,nums,0,{});
-        return ans;   
+        sort(arr.begin(),arr.end());
+        helper(ans,arr,{},0);
+        sort(ans.begin(),ans.end());
+        return ans;
     }
 };
