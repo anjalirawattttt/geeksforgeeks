e5b68eda1e77f314c1a3ca1ccd2f36f0302d578c
Anjali Rawat <132810690+anjalirawattttt@users.noreply.github.com>
2025-05-29 14:51:18 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Subset II/subset-ii.cpp
@@ -0,0 +1,22 @@
+// User function Template for C++
+
+class Solution {
+public:
+    void helper(vector<vector<int>> &ans,vector<int>& nums,int i,vector<int> subset){
+        ans.push_back(subset);                
+            
+        for(int j=i;j<nums.size();j++){
+            if(j>i && nums[j]==nums[j-1])continue;
+            subset.push_back(nums[j]);
+            helper(ans,nums,j+1,subset);
+            subset.pop_back();
+        }
+    }
+
+    vector<vector<int>> printUniqueSubsets(vector<int>& nums) {
+        vector<vector<int>> ans;
+        sort(nums.begin(),nums.end());
+        helper(ans,nums,0,{});
+        return ans;   
+    }
+};
