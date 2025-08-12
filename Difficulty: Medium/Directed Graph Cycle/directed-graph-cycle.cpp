33b10473bc585abae82cd996ca43f838e7fc1148
Anjali Rawat <132810690+anjalirawattttt@users.noreply.github.com>
2025-08-12 22:14:08 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.cpp
+++ Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.cpp
@@ -1,86 +1,40 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
 class Solution {
   public:
-    vector<vector<int>> buildGraph(int V,vector<vector<int>> & edges){
-        vector<vector<int>> adj(V);
-        for(auto &edge:edges){
+    void buildGraph(vector<vector<int>> &adj,vector<vector<int>> &edges){
+        for(auto edge:edges){
             adj[edge[0]].push_back(edge[1]);
         }
-        return adj;
     }
     
-    vector<int> top(int V,vector<vector<int>> adj,vector<int>& inDegree,int &count){
-        queue<int> q;
-        vector<int> result;
+    bool dfs(int u,vector<vector<int>> &adj,vector<bool> &visited,vector<bool> &recStack){
+        visited[u]=true;
+        recStack[u]=true;
+        for(int v:adj[u]){
+            if(!visited[v]){
+                if(dfs(v,adj,visited,recStack))return true;
+            }
+            else if(recStack[v]){
+                return true;
+            }
+        }
+        recStack[u]=false;
+        return false;
+    }
+    
+    bool isCyclic(int V, vector<vector<int>> &edges) {
+        
+        vector<vector<int>> adj(V);
+        buildGraph(adj,edges);
+        vector<bool> visited(V,false);
+        vector<bool> recStack(V,false);
         
         for(int i=0;i<V;i++){
-            if(inDegree[i]==0)q.push(i);
+            if(!visited[i]){
+                if(dfs(i,adj,visited,recStack))return true;
+            }
         }
-        while( !q.empty() ){
-            int u=q.front();
-            q.pop();
-            count++;
-            result.push_back(u);
-            for(int v:adj[u]){
-                inDegree[v]--;
-                if(inDegree[v]==0)q.push(v);
-            } 
-        }
-        return result;
-    }
-    
-    
-    bool isCyclic(int V, vector<vector<int>>& edges) {
-        // code here
-       
-        vector<vector<int>> adj=buildGraph(V,edges);
-        vector<int> inDegree(V,0);
-        int count=0;
-        vector<int>sorted;
         
-        
-        for(auto & edge:edges){
-            inDegree[edge[1]]++;
-        }
-        sorted=top(V,adj,inDegree,count);
-        
-        
-        return count!=V;
+        return false;
         
     }
 };
-
-
-//{ Driver Code Starts.
-
-int main() {
-    int tc;
-    cin >> tc;
-    cin.ignore();
-    while (tc--) {
-        int V, E;
-        cin >> V >> E;
-        cin.ignore();
-        vector<vector<int>> edges;
-        for (int i = 1; i <= E; i++) {
-            int u, v;
-            cin >> u >> v;
-            edges.push_back({u, v});
-        }
-
-        Solution obj;
-        bool ans = obj.isCyclic(V, edges);
-        if (ans)
-            cout << "true\n";
-        else
-            cout << "false\n";
-    }
-    return 0;
-}
-// } Driver Code Ends
