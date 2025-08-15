f80332bbb58328eda3a1f6adf5955b061507fbc4
Anjali Rawat <132810690+anjalirawattttt@users.noreply.github.com>
2025-08-15 22:54:22 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Topological sort/topological-sort.cpp
+++ Difficulty: Medium/Topological sort/topological-sort.cpp
@@ -1,38 +1,30 @@
 class Solution {
   public:
-    void buildGraph(vector<vector<int>> &adj,vector<vector<int>> &edges){
-        for(auto edge:edges){
-            adj[edge[0]].push_back(edge[1]);
-        }
-    }
     vector<int> topoSort(int V, vector<vector<int>>& edges) {
-        vector<int> ts;
+        // code here
+        vector<vector<int>> adj(V);
+        vector<int> res;
         vector<int> inDegrees(V,0);
         for(auto edge:edges){
+            adj[edge[0]].push_back(edge[1]);
             inDegrees[edge[1]]++;
         }
-        
-        vector<vector<int>> adj(V);
-        buildGraph(adj,edges);
-        
         queue<int> q;
-        
         for(int i=0;i<V;i++){
             if(inDegrees[i]==0){
                 q.push(i);
             }
         }
-        
         while(!q.empty()){
-            int size=q.size();
-            int node=q.front();
+            int u=q.front();
             q.pop();
-            ts.push_back(node);
-            for(int v: adj[node]){
+            res.push_back(u);
+            for(int v:adj[u]){
                 inDegrees[v]--;
                 if(inDegrees[v]==0)q.push(v);
             }
         }
-        return ts;
+        
+        return res;
     }
 };
