3e8316f0ef85c16369c80dfa939de203fb3526a7
Anjali Rawat <132810690+anjalirawattttt@users.noreply.github.com>
2025-08-16 18:57:15 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Floyd Warshall/floyd-warshall.cpp
@@ -0,0 +1,20 @@
+// User function template for C++
+
+class Solution {
+  public:
+    void floydWarshall(vector<vector<int>> &dist) {
+        // i==j dist->0
+        //i->j edge exists dist->weight
+        // else dist->1e8
+        int V=dist.size();
+        for(int k=0;k<V;k++){
+            for(int i=0;i<V;i++){
+                for(int j=0;j<V;j++){
+                    if(dist[i][k]!=1e8 && dist[k][j]!=1e8){
+                        dist[i][j]=min(dist[i][j],dist[i][k]+dist[k][j]);     
+                    }
+                }
+            }
+        }
+    }
+};
