deb55c517309cf6183510421d2e48ab79c04e77b
Anjali Rawat <132810690+anjalirawattttt@users.noreply.github.com>
2025-08-21 14:36:17 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum Spanning Tree/minimum-spanning-tree.cpp
+++ Difficulty: Medium/Minimum Spanning Tree/minimum-spanning-tree.cpp
@@ -1,113 +1,32 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
 class Solution {
   public:
-    // Function to find sum of weights of edges of the Minimum Spanning Tree.
-    class DisjointSet{
-        vector<int> parent,rank;
-        public:
-        DisjointSet(int n){
-            parent.resize(n);
-            rank.resize(n,0);
-            for(int i=0;i<n;i++){
-                parent[i]=i;
-            }
-        }
-        int findParent(int node){
-            if(parent[node]==node)return node;
-            return parent[node]= findParent(parent[node]);
-            
+    int spanningTree(int V, vector<vector<int>>& edges) {
+        vector<vector<pair<int,int>>> adj(V);
+        for(auto &e:edges){
+            adj[e[0]].push_back({e[1],e[2]});
+            adj[e[1]].push_back({e[0],e[2]});
         }
         
-        void unionByRank(int u,int v){
-            int pu=findParent(u);
-            int pv=findParent(v);
-            if(pu==pv)return;
-            if(rank[pu]<rank[pv]){
-                parent[pu]=pv;
-            }
-            else if(rank[pu]>rank[pv]){
-                parent[pv]=pu;
-            }
-            else{
-                parent[pv]=pu;
-                rank[pu]++;
-            }
-        }
+        using T=tuple<int,int,int>;//weight,node,parent
+        priority_queue<T,vector<T>,greater<T>> pq;
+        vector<bool> visited(V,false);
+        pq.push({0,0,-1});
         
-    };
-    
-    // Function to find sum of weights of edges of the Minimum Spanning Tree.
-    int spanningTree(int V, vector<vector<int>> adj[]) {
-        // code here
-        vector<pair<int,pair<int,int>>> edges;
-        int mstWeight=0;
-        DisjointSet ds(V);
+        int ans=0;
         
-        for(int u=0;u<V;u++){
-            for(auto &it:adj[u]){
-                int v=it[0];
-                int w=it[1];
-                edges.push_back({w,{u,v}});
+        while(!pq.empty()){
+            auto [wt,u,p]=pq.top();
+            pq.pop();
+            if(visited[u])continue;
+            visited[u]=true;
+            ans+=wt;
+            for(auto &[v,w]:adj[u]){
+                if(!visited[v]){
+                    pq.push({w,v,u});
+                }    
             }
         }
-        sort(edges.begin(),edges.end());
+        return ans;
         
-        
-        
-        
-            for(auto &edge:edges){
-                int u=edge.second.first;
-                int v=edge.second.second;
-                int w=edge.first;
-                if(ds.findParent(u)!=ds.findParent(v)){
-                    ds.unionByRank(u,v);
-                    mstWeight+=w;
-                }
-            }
-            
-        
-        return mstWeight;
     }
 };
-
-
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        int V, E;
-        cin >> V >> E;
-        vector<vector<int>> adj[V];
-        int i = 0;
-        while (i++ < E) {
-            int u, v, w;
-            cin >> u >> v >> w;
-            vector<int> t1, t2;
-            t1.push_back(v);
-            t1.push_back(w);
-            adj[u].push_back(t1);
-            t2.push_back(u);
-            t2.push_back(w);
-            adj[v].push_back(t2);
-        }
-
-        Solution obj;
-        cout << obj.spanningTree(V, adj) << "\n";
-
-        cout << "~"
-             << "\n";
-    }
-
-    return 0;
-}
-
-// } Driver Code Ends
