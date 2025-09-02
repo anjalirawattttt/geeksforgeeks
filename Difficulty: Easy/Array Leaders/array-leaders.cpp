9dda7734337107d43e06946c5913469cf268148e
Anjali Rawat <132810690+anjalirawattttt@users.noreply.github.com>
2025-09-02 13:58:47 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Array Leaders/array-leaders.cpp
+++ Difficulty: Easy/Array Leaders/array-leaders.cpp
@@ -1,68 +1,19 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
 
 
 class Solution {
     // Function to find the leaders in the array.
   public:
     vector<int> leaders(vector<int>& arr) {
-        // Code here
-        vector<int> l;
-        int maxx=arr[arr.size()-1];
-        for(int i=arr.size()-1;i>=0;i--){
-            maxx=max(maxx,arr[i]);
-            if(maxx==arr[i]){
-                l.push_back(arr[i]);
+        vector<int> ans;
+        int n=arr.size();
+        int maxEle=-1;
+        for(int i=n-1;i>=0;i--){
+            maxEle=max(maxEle,arr[i]); 
+            if(arr[i]==maxEle){
+                ans.push_back(arr[i]);
             }
-            
         }
-        reverse(l.begin(),l.end());
-        
-        return l;
+        reverse(ans.begin(),ans.end());
+        return ans;
     }
 };
-
-
-//{ Driver Code Starts.
-
-int main() {
-    int t; // Number of test cases
-    cin >> t;
-    cin.ignore(); // Ignore the newline after the integer input
-    while (t--) {
-        vector<int> a;
-        string input;
-
-        // Input format: first number n followed by the array elements
-        getline(cin, input);
-        stringstream ss(input);
-        int num;
-        while (ss >> num)
-            a.push_back(num); // Read the array elements from input string
-
-        Solution obj;
-        vector<int> result = obj.leaders(a);
-
-        // Print the result in the required format
-        if (result.empty()) {
-            cout << "[]"; // Print empty brackets if no leaders are found
-        } else {
-            for (int i = 0; i < result.size(); i++) {
-                if (i != 0)
-                    cout << " ";
-                cout << result[i];
-            }
-        }
-        cout << endl;
-        cout << "~" << endl;
-    }
-
-    return 0;
-}
-
-// } Driver Code Ends
