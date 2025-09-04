a1f16f3681fa38bfa84618f641e39cce2c38bfe8
Anjali Rawat <132810690+anjalirawattttt@users.noreply.github.com>
2025-09-04 12:35:47 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Allocate Minimum Pages/allocate-minimum-pages.cpp
+++ Difficulty: Medium/Allocate Minimum Pages/allocate-minimum-pages.cpp
@@ -1,35 +1,38 @@
 class Solution {
   public:
     int findPages(vector<int> &arr, int k) {
-        int maxEle=0,sum=0;
-        for(int i=0;i<arr.size();i++){
-            maxEle=max(maxEle,arr[i]);
+        int n=arr.size();
+        if(k>n)return -1;
+        
+        int maxPages=0,sum=0;
+        for(int i=0;i<n;i++){
             sum+=arr[i];
+            maxPages=max(maxPages,arr[i]);
         }
+        int ans=-1;
         
-        if(k>arr.size())return -1;
-        
-        int l=maxEle,r=sum;
-        int ans=sum;
+        int l=maxPages,r=sum;
         while(l<=r){
-            int mid=l+(r-l)/2;
-            int s=1,temp=arr[0];
-            for(int i=1;i<arr.size();i++){
-                if(temp+arr[i]<=mid){
-                    temp+=arr[i];
-                }  
+            int mid = l +(r-l)/2;
+            int s=1,temp=0;
+            
+            for(int i=0;i<n;i++){
+                if(temp+arr[i]<=mid)temp+=arr[i];  
                 else{
                     s++;
                     temp=arr[i];
                 }
             }
+            
             if(s<=k){
-                ans=min(ans,mid);
-                r=mid-1;
+                ans=mid;
+                r=mid-1;    
             }
-            else l=mid+1;
+            else{
+                l=mid+1;    
+            }
+            
         }
         return ans;
-        
     }
 };
