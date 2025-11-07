efcfbbc435870f482800414a3b141468aff74379
Anjali Rawat <132810690+anjalirawattttt@users.noreply.github.com>
2025-11-07 11:16:42 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Missing And Repeating/missing-and-repeating.cpp
+++ Difficulty: Easy/Missing And Repeating/missing-and-repeating.cpp
@@ -1,61 +1,25 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-
-using namespace std;
-
-
-// } Driver Code Ends
-
 class Solution {
   public:
     vector<int> findTwoElement(vector<int>& arr) {
-        // code here
-        int miss=-1,rep=-1;
-        
-        for(int i=0;i<arr.size();i++){
-            int val=abs(arr[i]);
-            if(arr[val-1]>0){
-                arr[val-1]=-arr[val-1];  
+        int n=arr.size();
+        int repeat=-1,missing=-1;
+        for(int i=0;i<n;i++){
+            int idx=abs(arr[i])-1;
+            if(arr[idx]<0){
+                repeat=idx+1;    
             }
             else{
-                rep=val;
+                arr[idx]=-arr[idx];
             }
         }
         
-        for(int i=0;i<arr.size();i++){
+        for(int i=0;i<n;i++){
             if(arr[i]>0){
-                miss=i+1;
+                missing=i+1;
                 break;
             }
         }
-        return {rep,miss};
         
+        return {repeat,missing};
     }
 };
-
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    cin.ignore();
-    while (t--) {
-        string input;
-        int num;
-        vector<int> arr;
-        getline(cin, input);
-        stringstream s2(input);
-        while (s2 >> num) {
-            arr.push_back(num);
-        }
-        Solution ob;
-        auto ans = ob.findTwoElement(arr);
-        cout << ans[0] << " " << ans[1] << "\n";
-
-        cout << "~"
-             << "\n";
-    }
-    return 0;
-}
-// } Driver Code Ends
